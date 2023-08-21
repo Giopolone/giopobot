bd8d14f18f5f8c262bd716692d615caacfa5a6af
Giopolone <91971573+Giopolone@users.noreply.github.com>
2023-08-21 17:26:43 +0200

    new Update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const client = new Discord.Client(
     {intents:["GUILDS","GUILD_MEMBERS","GUILD_MESSAGES","GUILD_MESSAGE_REACTIONS","DIRECT_MESSAGES","AUTO_MODERATION_CONFIGURATION"]}
 )
 
-client.login("MTE0MzExMjcxNTExMjU1MDU1Mg.GW1Z03.J5npmOX-muvPNAXce7hAOjnjx8GG7ZC2THLDQA")
+client.login("process.env.token")
 
 client.on("ready", ()=> {
     console.log("Bot ONLINE")
